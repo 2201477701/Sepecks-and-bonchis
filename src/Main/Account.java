2717ee011a77c31a5dada3789f2fc0ec92644468
Junaid <220147701@mycput.ac.za>
2025-03-17 12:04:04 +0200

    imtrying

---
+++ src/Main/Account.java
@@ -0,0 +1,39 @@
+package Main;
+
+public class Account {
+
+    public String accountNumber;
+    public String accountType;
+    public Double balance;
+
+    public Account() {}
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount;
+
+    }
+}
